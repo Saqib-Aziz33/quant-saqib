c1eb67be46e58e418613134d16cb40fb0c55ae0d
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-04 07:46:12 -0700

    overflow fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import {useEffect} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 // layout
 import Base from "./components/layouts/Base";
+// aos
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // other
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.min.js'
 import "./app.scss";
 
 function App() {
+  useEffect(() => {
+    AOS.init({})
+  }, [])
   return (
     <BrowserRouter>
       <Base>
--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section className="hero">
         <div className="container ps-5 ps-md-0">
-            <h1 className="display-1 fw-bold mb-4">The future <br /> of finance. <br /> <span className="text-grad">Today.</span></h1>
+            <h1 className="display-1 fw-bold mb-4">The future <br /> of finance <br /> <span className="text-grad">Today.</span></h1>
             <Button>Get started</Button>
         </div>
         <video src="assets/hero-video.mp4" autoPlay muted loop />
