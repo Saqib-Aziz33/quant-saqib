549634fca44ddb391c709463ff1ae23e870b171c
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2022-10-04 07:15:03 -0700

    create value section

--- src/components/elements/CreateValueSec.jsx
+++ src/components/elements/CreateValueSec.jsx
@@ -1,13 +1,39 @@
 function CreateValueSec() {
   return (
-    <section className="container">
-        h2
-        <div className="row">
-            <section className="col-md-4"></section>
+    <section className="container my-5 create-value-section">
+        <h2 className="display-4 fw-bold text-center">Create value, reduce <br /> costs, mitigate risk</h2>
+        <div className="row align-items-center my-5">
             <section className="col-md-4">
+                <div className="box">
+                    <h6>Capital markets</h6>
+                    <small>Assets of all kinds – both physical and digital – are being tokenised. Issuers are gaining access to entire new pools of liquidity, while investors are diversifying their portfolios in ways they could only dream of just a few years ago.</small>
+                </div>
+                <div className="box ms-0 ms-md-5">
+                    <h6>Capital markets</h6>
+                    <small>Assets of all kinds – both physical and digital – are being tokenised. Issuers are gaining access to entire new pools of liquidity, while investors are diversifying their portfolios in ways they could only dream of just a few years ago.</small>
+                </div>
+                <div className="box">
+                    <h6>Capital markets</h6>
+                    <small>Assets of all kinds – both physical and digital – are being tokenised. Issuers are gaining access to entire new pools of liquidity, while investors are diversifying their portfolios in ways they could only dream of just a few years ago.</small>
+                </div>
+            </section>
+            <section className="col-md-4 d-none d-md-block">
                 <img className='img-fluid' src='assets/quant.svg' alt='' />
             </section>
-            <section className="col-md-4"></section>
+            <section className="col-md-4">
+            <div className="box ms-0 ms-md-5">
+                    <h6>Capital markets</h6>
+                    <small>Assets of all kinds – both physical and digital – are being tokenised. Issuers are gaining access to entire new pools of liquidity, while investors are diversifying their portfolios in ways they could only dream of just a few years ago.</small>
+                </div>
+                <div className="box">
+                    <h6>Capital markets</h6>
+                    <small>Assets of all kinds – both physical and digital – are being tokenised. Issuers are gaining access to entire new pools of liquidity, while investors are diversifying their portfolios in ways they could only dream of just a few years ago.</small>
+                </div>
+                <div className="box ms-0 ms-md-5">
+                    <h6>Capital markets</h6>
+                    <small>Assets of all kinds – both physical and digital – are being tokenised. Issuers are gaining access to entire new pools of liquidity, while investors are diversifying their portfolios in ways they could only dream of just a few years ago.</small>
+                </div>
+            </section>
         </div>
     </section>
   )
